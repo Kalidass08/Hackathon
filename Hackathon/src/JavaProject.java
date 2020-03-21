82a76bdf873046fb86d5c6e49ca7419adc54a91e
Kalidass <kalidass08031999@gmail.com>
2020-03-21 10:37:11 +0530

    Final Submit

--- Hackathon/src/JavaProject.java
+++ Hackathon/src/JavaProject.java
@@ -6,8 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DecimalFormat;
+import java.math.BigDecimal;
+
 
 import org.json.*;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -16,8 +19,14 @@ import com.mysql.cj.xdevapi.JsonArray;
 import java.util.StringTokenizer;
 
 public class JavaProject {
+	BigDecimal formatchanger(String par) {
+		BigDecimal value=new BigDecimal(par);
+		value=value.setScale(2,BigDecimal.ROUND_HALF_UP);
+		return value;
+	}
 	public static void main(String[] args) throws IOException, SQLException {
 		DecimalFormat df = new DecimalFormat("#.##");
+		JavaProject parse = new JavaProject();
 		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaProject","root","Kalidass@0803");
 		System.out.println("Database connected\n");
 		Statement stm=conn.createStatement();
@@ -30,6 +39,7 @@ public class JavaProject {
 		int max=0;
 		int count=0;
 		int avg;
+		JSONArray array=new JSONArray();
 		JSONObject main=new JSONObject();
 		JSONObject json = new JSONObject();
 		JSONObject obj=new JSONObject();
@@ -52,17 +62,21 @@ public class JavaProject {
 			String k=rs.getString("transaction");
 			float v=Float.parseFloat(df.format(rs.getFloat("time")));
 			obj.put(k,v);
-			count++;
-			add=add+(int)v;
-			if(max<(int)v);
-			{
-				max=(int)v;
-			}
 		}
-		avg=add/count;
-		json.put("Avg:",avg);
-		json.put("Max",max);
-		main.put(json,obj);
+		json.put("value",obj);
+		main.put("Total Transaction",json);
+		rs = stm.executeQuery("select max(time) from data");
+		if(rs.next())
+		{
+			json.put("MaxValue",parse.formatchanger(rs.getString(1)));
+		}
+		rs = stm.executeQuery("select avg(time) from data"); 
+		if(rs.next())
+		{
+			json.put("AvgValue",parse.formatchanger(rs.getString(1)));
+		}
+		
+		array.add(0,main);
 		FileWriter file1=new FileWriter("op.json");
 		file1.write(main.toString());
 		file1.close();
